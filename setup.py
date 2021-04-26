b458feacb66cd2d6fa29e5638917275246aa00c5
Guo Zhang <zhangguo@quanttide.com>
2021-04-26 19:42:24 +0800

    create

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-wechat-login',
+    version='0.0.1-dev',
+    packages=['django_wechat_login'],
+    url='',
+    license='Apache 2.0',
+    author='QuantTide Inc.',
+    author_email='open@quanttide.com',
+    description='微信登录Django服务端SDK'
+)
